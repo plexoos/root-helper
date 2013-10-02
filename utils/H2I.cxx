160d7f614e5fb8241112e6d1ab9d3c55a3453c01
Dmitri Smirnov <d.s@plexoos.com>
2013-10-02 10:58:23 -0400

    Added a method to calculate the integral of bins above function y=f(x) provided by the user

--- utils/H2I.cxx
+++ utils/H2I.cxx
@@ -1,5 +1,9 @@
 #include "H2I.h"
 
+#include "TF1.h"
+#include "TList.h"
+#include "TText.h"
+
 
 ClassImp(rh::H2I)
 
@@ -17,6 +21,39 @@ H2I::H2I(string name, string title, Int_t nbinsx, Double_t xlow, Double_t xup, I
    TH2I(name.c_str(), title.c_str(), nbinsx, xlow, xup, nbinsy, ylow, yup)
 {
    SetOption(options.c_str());
+}
+
+
+
+/**
+ * Computes integral value of all bin above the curve given by the function f1.
+ */
+double H2I::CalcIntegralAbove(TF1& f1)
+{
+   int xfirst = fXaxis.GetFirst();
+   int xlast  = fXaxis.GetLast();
+   int yfirst = fYaxis.GetFirst();
+   int ylast  = fYaxis.GetLast();
+
+   double integral = 0;
+
+   for (Int_t iBinX=xfirst; iBinX<=xlast; ++iBinX)
+   {
+      for (Int_t iBinY=yfirst; iBinY<=ylast; ++iBinY)
+      {
+         double binXCenter = fXaxis.GetBinCenter(iBinX);
+         double binYCenter = fYaxis.GetBinCenter(iBinY);
+
+         double funcVal = f1.Eval(binXCenter);
+
+         printf("iBinX: %d, iBinY: %d, %f, %f, %f\n", iBinX, iBinY, binXCenter, binYCenter, funcVal);
+
+         if (binYCenter < funcVal) continue;
+         integral += GetBinContent(iBinX, iBinY);
+      }
+   }
+
+   return integral;
 }
 
 }
